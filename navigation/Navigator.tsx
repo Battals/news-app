cfdaa38ca5010c0f5b1c5ccab5a4fda1b0d545e5
Battals <batt0006@stud.kea.dk>
2023-05-16 02:20:33 +0200

    Commit

--- navigation/Navigator.tsx
+++ navigation/Navigator.tsx
@@ -4,22 +4,35 @@ import { NativeScreenNavigationContainer } from 'react-native-screens'
 import { View, Text } from 'react-native'
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import NewsOverview from '../screens/NewsOverview';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Saved from '../screens/Saved';
 
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function HomeScreen(){
-    return <View>
-        <Text>
-            Home
-        </Text>
-    </View>
+
+return (<Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Saved" component={Saved} />
+</Tab.Navigator>
+)
 }
 export default function AppNavigator(){
     return (
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name='HomeScreen' component={HomeScreen} />
+        <Stack.Screen
+        options={{headerShown: false}}
+            name='HomeScreen'
+             component={HomeScreen} />
+            <Stack.Screen
+            name='NewsOverview'
+             component={NewsOverview} />
+             
         </Stack.Navigator>
     </NavigationContainer>
     );
---
+++ screens/Saved.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Saved = () => {
+  return (
+    <View>
+      <Text >Hello, React Native!</Text>
+    </View>
+  );
+};
+
+export default Saved;
